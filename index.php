00db29f244348cf1053a2872b5cd1d4ea5007289
dymaxionkim <dymaxion.kim@gmail.com>
2017-03-14 11:11:24 +0900

    php 연결 위주로 수정 중

--- index.php
+++ index.php
@@ -6,15 +6,23 @@
   <link rel="stylesheet" type="text/css" href="style.css">
 </head>
 <body>
-  <img src="http://hubright.cafe24.com/wp-content/uploads/2017/01/hubright_logo_sticky.png">
-  <h1>ERP_CODE for DJDMP</h1>
-  <a href="https://try.gitea.io/dymaxionkim/test/raw/master/sample10.pdf">link</a>
+
+  <header>
+    <img src="http://hubright.cafe24.com/wp-content/uploads/2017/01/hubright_logo_sticky.png">
+    <h1>ERP_CODE for DJDMP</h1>
+  </header>
 
   <nav>
     <ol>
-      <li><a href="Generate.php">Generate</a></li>
-      <li><a href="List.php">List<a/></li>
+      <?php echo file_get_contents("nav.php"); ?>
     </ol>
   </nav>
+
+  <article>
+    <?php if ( empty($_GET['id']) == false ) {
+      echo file_get_contents($_GET['id'].".php");
+    } ?>
+  </article>
+
 </body>
 </html>
---
+++ nav.php
@@ -0,0 +1,2 @@
+<li><a href="index.php?id=Generate">Generate</a></li>
+<li><a href="index.php?id=List">List<a/></li>
